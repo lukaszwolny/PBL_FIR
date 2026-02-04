3255c6b6092748d887753bb1ac59652689db8226
Karol <knieroba2001@gmail.com>
2026-02-04 19:00:48 +0100

    testy apb: -testy rejestrów kontrolnych

--- cocotb/apb_testy/apb_tb2.py
+++ cocotb/apb_testy/apb_tb2.py
@@ -138,3 +138,40 @@ factory = TestFactory(test_function=write_read_random_ram_and_ratio_test)
 factory.add_option("ratio", [3.0, 4.0, 6.0])  # Różne ratio zegarów do przetestowania
 factory.generate_tests()
 
+@cocotb.test()
+async def test_control_registers_readback(dut):
+    tester = APB_SoC_Tester(dut)
+    # Ustawiamy szybszy clk_b dla sprawnego działania CDC
+    await tester.setup(speed_ratio=3.0)
+
+    # Definicja adresów na podstawie dekodera (CDC_A[5]=1 -> 0x20 + nr_rejestru)
+    REG_START      = 0x20 # nr_Rejestru 0
+    REG_DONE       = 0x21 # nr_Rejestru 1
+    REG_PRACUJE    = 0x22 # nr_Rejestru 2
+    REG_ILE_WSP    = 0x23 # nr_Rejestru 3
+    REG_ILE_PROBEK = 0x24 # nr_Rejestru 4
+
+    ilosc_wsp = 15
+    ilosc_probek = 100
+
+    await tester.apb.write(REG_ILE_WSP, ilosc_wsp)
+    await tester.apb.write(REG_ILE_PROBEK,ilosc_probek)
+
+    raw_wsp = await tester.apb.read(REG_ILE_WSP)  
+    val_wsp = int.from_bytes(raw_wsp, byteorder='little') & 0xFFFF  # Bezpieczna konwersja bajtów na int
+    assert val_wsp == ilosc_wsp, f"Blad! Oczekiwano {ilosc_wsp}, otrzymano {val_wsp}"
+        
+    raw_probek = await tester.apb.read(REG_ILE_PROBEK)
+    val_probek = int.from_bytes(raw_probek, byteorder='little') & 0xFFFF
+    assert val_probek == ilosc_probek, f"Blad! Oczekiwano {ilosc_probek}, otrzymano {val_probek}"
+
+
+    #--- Specjalny przypadek: START ---
+    #Rejestr START (0x20) w Twoim RTL sam się zeruje: if(rej[0][0]) rej[0][0] <= 1'b0;
+    await tester.apb.write(REG_START, 0x0001)
+    
+    await Timer(100, units="ns") # Czekamy aż logika clk_b go wyzeruje
+    
+    raw_start = await tester.apb.read(REG_START)
+    val_start = int.from_bytes(raw_start, byteorder='little') & 0xFFFF
+    assert val_start == 0, "Błąd! Rejestr START nie wyzerował się automatycznie"
